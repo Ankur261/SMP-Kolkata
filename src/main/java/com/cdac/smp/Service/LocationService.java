b11390f6fc9d9175ab83833e96270fce6d7a80b4
Pallavi14-S <pallavishingote142@gmail.com>
2025-09-25 10:24:23 +0530

    Location Folder Structure

---
+++ src/main/java/com/cdac/smp/Service/LocationService.java
@@ -0,0 +1,11 @@
+package com.cdac.smp.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+
+public class LocationService {
+
+}

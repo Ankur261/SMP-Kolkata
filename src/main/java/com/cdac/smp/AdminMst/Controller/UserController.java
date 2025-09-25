75638589f8beffe5b75ce3b038e55163b2511055
Rahul25052002 <rnikale55@gmail.com>
2025-09-25 10:14:06 +0530

    Basic template create for the USer Module

---
+++ src/main/java/com/cdac/smp/AdminMst/Controller/UserController.java
@@ -0,0 +1,11 @@
+package com.cdac.smp.AdminMst.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	
+}

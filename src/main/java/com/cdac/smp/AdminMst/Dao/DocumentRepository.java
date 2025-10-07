6d45a8bc639c83372f2a320d2dfdc05614fe2315
unknown <Pallavi@cmkl-pallavi.cdacmumbai.in>
2025-10-07 16:53:47 +0530

    All done

--- src/main/java/com/cdac/smp/AdminMst/Dao/DocumentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cdac.smp.AdminMst.Dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cdac.smp.AdminMst.Model.Documents;
-
-public interface DocumentRepository extends MongoRepository<Documents, String> {
-}
